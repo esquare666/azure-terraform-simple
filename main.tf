ad3e9663000d5a765e9c0edc6c92c8ec0363278b
esquare666 <elayaraja.kv@gmail.com>
2020-05-26 21:34:28 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,4 +17,8 @@ resource "azurerm_virtual_network" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   address_space       = ["10.0.0.0/16"]
+  
+   tags = {
+        user_name = var.tag_user_name
+  }
 }
