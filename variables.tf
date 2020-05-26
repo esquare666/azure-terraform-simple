8d3f724c01a85b423d4bc20d69176624999a6c6d
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 10:55:33 +1200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,20 +10,11 @@ variable "azure_region" {
 
 variable "cidrs" { default = ["10.0.0.0/8"] }
 
-variable "subnet_app1" { 
-  type = string
-  default = "10.0.0.0/24"
-  }
-
-variable "subnet_app2" { 
-  type = string
-  default = "10.0.1.0/24"
-  }
-
-variable "subnet_app3" { 
-  type = string
-  default = "10.0.2.0/24"
-  }
+variable "subnet_app" {
+  description = "Application Subnets"
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.1.0/24", "10.0.2.0/24"]
+}
 
 variable "tag_user_name" {
   type = string
