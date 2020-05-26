6232b23783fe38045d1b276801def2e40a9f4028
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 11:00:08 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,9 +24,9 @@ resource "azurerm_virtual_network" "main" {
 }
 
 resource "azurerm_subnet" "app" {
-  count = 3
+  count = length(var.subnet_app)
   name                 = "${var.prefix}-app${count.index}"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefix     = "${var.subnet_app}${count.index}"
+  address_prefix     = "${var.subnet_app}${var.subnet_app[count.index]}"
 }
