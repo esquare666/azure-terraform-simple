5e8e1ef4f5ae12cc2fd7a7a9472db583698b471b
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 11:29:02 +1200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "Resource Group ID" {
+output "Resource_Group_ID" {
   description = "The ID of the Virtual Network"
   value       = azurerm_resource_group.main.id
 }
 
-output "ARN of App Subnets" {
+output "ARN_App_Subnets" {
   value       = azurerm_subnet.app[*].arn
   description = "The ARNs for all subnets"
 }
