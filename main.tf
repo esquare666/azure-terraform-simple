c2f58866ec5bf55c96c22b0c95e9211e9adbb0cf
esquare666 <elayaraja.kv@gmail.com>
2020-05-26 21:21:50 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version = "=1.44.0"
+  version = "~> 1.44"
 
   subscription_id = "d52515da-685f-492c-8a14-0fdc40819e4f"
   tenant_id       = "ecf8325f-91c3-4262-8fcf-8ca81cd0f45e"
