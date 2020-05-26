606f55d427a2ae9528c90c241772752249e35bdf
esquare666 <elayaraja.kv@gmail.com>
2020-05-26 20:51:28 +1200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "prefix" {
+  type    = string
+  default = "esquare666"
+}
+
+variable "azure_region" {
+  type    = string
+  default = "australiaeast"
+}
+
+variable "tag_user_name" {
+  type = string
+  default = "Elayaraja Kathirvel"
+}
