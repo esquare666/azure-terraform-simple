8835b3d7dd837404eea15fff3603631d416210de
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 10:10:22 +1200

    Default CIDR

--- variables.tf
+++ variables.tf
@@ -8,7 +8,7 @@ variable "azure_region" {
   default = "australiaeast"
 }
 
-variable "cidrs" { default = [] }
+variable "cidrs" { default = ["10.0.0.0/8"] }
 
 variable "tag_user_name" {
   type = string
