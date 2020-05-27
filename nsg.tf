82da1f4f79517cf96bc252251418dc0285a542c1
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 14:29:35 +1200

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,4 +1,4 @@
-esource "azurerm_network_security_group" "main" {
+esource "azurerm_network_security_group" "app" {
   name                = "${var.prefix}-app"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
@@ -21,7 +21,21 @@ esource "azurerm_network_security_group" "main" {
   }
 }
 
-resource "azurerm_network_interface_security_group_association" "main" {
-  network_interface_id      = azurerm_network_interface.main.id
-  network_security_group_id = azurerm_network_security_group.main.id
+# NSG association with Network Interface
+#resource "azurerm_network_interface_security_group_association" "main" {
+#  network_interface_id      = azurerm_network_interface.main.id
+#  network_security_group_id = azurerm_network_security_group.app.id
+#}
+
+# NSG association with single Subnet
+#resource "azurerm_subnet_network_security_group_association" "main" {
+#  subnet_id                 = azurerm_subnet.app.id
+#  network_security_group_id = azurerm_network_security_group.app.id
+#}
+
+# NSG association with Multiple Subnet
+resource "azurerm_subnet_network_security_group_association" "app" {
+  count = length(var.subnet_app)
+  subnet_id                 = azurerm_subnet.app[count.index].id
+  network_security_group_id = azurerm_network_security_group.app.id  
 }
