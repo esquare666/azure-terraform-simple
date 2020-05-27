2758f291b51a05b11d006cd6170ef74dbf6070f9
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 14:31:48 +1200

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -1,4 +1,4 @@
-esource "azurerm_network_security_group" "app" {
+resource "azurerm_network_security_group" "app" {
   name                = "${var.prefix}-app"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
