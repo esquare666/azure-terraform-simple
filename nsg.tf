98f859352cc81a412843e61dea9000fdbce34e0d
esquare666 <elayaraja.kv@gmail.com>
2020-05-27 14:37:01 +1200

    Update nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -4,13 +4,13 @@ resource "azurerm_network_security_group" "app" {
   resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
-    name                       = "${var.prefix}-app-ssh-http-https"
+    name                       = "${var.prefix}-app-ssh"
     priority                   = 100
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "22,80,443"
+    destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
